93467e3003cf62d0633978f5e3c90f1358e3702d
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-02-29 22:35:59 +0100

    update lambda handler

--- utils/handlers/lambda.go
+++ utils/handlers/lambda.go
@@ -1,21 +1,95 @@
 package handlers
 
 import (
-	// "github.com/openimw/smtpless/utils"
-	"github.com/aws/aws-lambda-go/lambda"
+	"os"
+	"encoding/base64"
+	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/openimw/smtpless/utils"
 )
 
+type JsonBody map[string]string
+
+type Response struct {
+	Status  int
+	Headers map[string]string
+	Body    JsonBody
+}
+
+func response(res Response) (events.APIGatewayProxyResponse, error) {
+
+	body, err := json.Marshal(res.Body)
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(body),
+		Headers:    res.Headers,
+		StatusCode: res.Status,
+	}, err
+}
 
 func handle(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	return events.APIGatewayProxyResponse{
-		Body: "Hello",
-		StatusCode: 200,
-	}, nil
+	body, err := base64.StdEncoding.DecodeString(request.Body)
+
+	if err != nil {
+		return response(Response{
+			Status: 403,
+			Body: JsonBody{
+				"type":    "error",
+				"message": "Invalid body data",
+			},
+		})
+	}
+
+	var re utils.RawEmail
+
+	err = json.Unmarshal(body, &re)
+
+	if err != nil {
+		return response(Response{
+			Status: 403,
+			Body: JsonBody{
+				"type":    "error",
+				"message": "Invalid json body",
+			},
+		})
+	}
+
+	email := utils.Email{
+		To:   "test",
+		Body: "body",
+	}
+
+	smtp := utils.SmtpConfig{
+		Host:     os.Getenv("EMAIL_HOST"),
+		Port:     os.Getenv("EMAIL_PORT"),
+		From:     os.Getenv("EMAIL_FROM"),
+		Username: os.Getenv("EMAIL_USERNAME"),
+		Password: os.Getenv("EMAIL_PASSWORD"),
+	}
+
+	err = utils.Send(email, smtp)
+
+	if err != nil {
+		return response(Response{
+			Status: 403,
+			Body: JsonBody{
+				"type":    "error",
+				"message": "Cannot send email",
+			},
+		})
+	}
+
+	return response(Response{
+		Status: 200,
+		Body: JsonBody{
+			"type":    "success",
+			"message": "Email sent successfully.",
+		},
+	})
 }
 
 func LambdaHandler() {
 	lambda.Start(handle)
 }
-
